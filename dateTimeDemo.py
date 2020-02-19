bf0e686afda68c782bcfc1ed46278b735b8a9f85
DESKTOP-FEURCKB\yyt_c <liutao910612@126.com>
2020-02-19 09:08:51 +0800

    Complete datatime Add regex

--- dateTimeDemo.py
+++ dateTimeDemo.py
@@ -1,6 +1,8 @@
 import datetime
 
 # today()返回一个当前本地时间的datetime对象
+import time
+
 import pytz
 
 print("today is : ",datetime.datetime.today()) # 2020-02-18 21:24:48.838799
@@ -10,5 +12,16 @@ print("now is : ",datetime.datetime.now(pytz.timezone('Asia/Shanghai'))) # 2020-
 
 # utcnow() 返回utc时间的datetime对象
 print("utcnow is : ",datetime.datetime.utcnow()) # 2020-02-18 13:33:04.378297
+
+# fromtimestamp(timestamp[,tz]) 根据时间戳创建一个datatime对象 ， 其中tz是时区
+print("fromtimestamp is : ",datetime.datetime.fromtimestamp(time.time())) # 2020-02-19 08:27:22.909740
+
+# utcfromtimestamp(timestamp[,tz]) 根据时间戳创建一个utc的datatime对象
+print("utcfromtimestamp is : ",datetime.datetime.utcfromtimestamp(time.time())) # 2020-02-19 00:27:22.909740
+
+# strptime(date_string,format) 格式化时间字符串，而strftime(format)和strptime的用法正好相反，是将格式字符串转换为datatime对象
+dt = datetime.datetime.now()
+print("strptime is : ",datetime.datetime.strptime(str(dt),"%Y-%m-%d %H:%M:%S.%f")) # 2020-02-19 08:30:49.806621
+
 
 
